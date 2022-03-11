2c9f0716d9d3a24c3a9f9a98b10c3bb922e6e9da
Shrirang Dharkar <dharkarshrirang@gmail.com>
2022-03-11 22:27:33 +0530

    Comitting build.yml & sonarCloud properties

--- src/main/java/com/DealsFinder/Deals/DealsApplication.java
+++ src/main/java/com/DealsFinder/Deals/DealsApplication.java
@@ -3,7 +3,10 @@ package com.DealsFinder.Deals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+//@EnableSwagger2
 public class DealsApplication {
 
 	public static void main(String[] args) {
